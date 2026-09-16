73ae8d4bbae9d42aaa39f255f84310bf37d80618
DinhHoangMy1 <my.2474802010245@vanlanguni.vn>
2026-09-16 19:20:15 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 // Updated login logic on feature branch
 function login(username, password) {
-  return username === 'admin' && password === '9999';
+  return username === 'admin' && password === '123';
 }
 
 module.exports = { login };
